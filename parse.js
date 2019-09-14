4055cd9c0589f0d184f4c604191f14509db3d1d4
John Mitsch <jomitsch@redhat.com>
2019-09-14 17:59:18 -0400

    Looping through packets

--- parse.js
+++ parse.js
@@ -9,38 +9,13 @@ const checkMagicNumber = buffer => {
   }
 };
 
-/**
- * Global header format according to pcap-savefile. Each row is 4 bytes, the boxes
- * in the split rows are two bytes each.
-            +------------------------------+
-            |        Magic number          |
-            +--------------+---------------+
-            |Major version | Minor version |
-            +--------------+---------------+
-            |      Time zone offset        |
-            +------------------------------+
-            |     Time stamp accuracy      |
-            +------------------------------+
-            |       Snapshot length        |
-            +------------------------------+
-            |   Link-layer header type     |
-            +------------------------------+
-* */
-const parseGlobalHeader = buffer => {
-  const headerSeparations = {
-    magicNumber: [0, 4],
-    majorVersion: [4, 6],
-    minorVersion: [6, 8],
-    timeZoneOffset: [8, 12],
-    timeStampAccuracy: [12, 16],
-    snapshotLength: [16, 20],
-    linkLayerHeaderType: [20, 24]
-  };
-
+const parseHeadersFromBuffer = (buffer, headerSeparations, offset = 0) => {
   const parsedHeaders = {};
 
   for (const [headerKey, separators] of Object.entries(headerSeparations)) {
-    const headerBuffer = buffer.slice(...separators);
+    const bufferStart = offset + separators[0];
+    const bufferEnd = offset + separators[1];
+    const headerBuffer = buffer.slice(bufferStart, bufferEnd);
     const byteLength = separators[1] - separators[0];
 
     // the magic number shouldn't be converted to an int, we can just make sure the bytes are correct
@@ -53,56 +28,83 @@ const parseGlobalHeader = buffer => {
   return parsedHeaders;
 };
 
-/**
- * Packet header format according to pcap-savefile. Each row is 4 bytes
-              +----------------------------------------------+
-              |          Time stamp, seconds value           |
-              +----------------------------------------------+
-              |Time stamp, microseconds or nanoseconds value |
-              +----------------------------------------------+
-              |       Length of captured packet data         |
-              +----------------------------------------------+
-              |   Un-truncated length of the packet data     |
-              +----------------------------------------------+
-* */
-const parsePacketHeader = (buffer, packetHeaderStart) => {
-  const headerSeparators = {
-    timeStampSeconds: [0, 4],
-    timeStampMicro: [4, 8],
-    length: [8, 12],
-    lengthUntruncated: [12, 16]
-  };
-
-  const parsedHeaders = {};
-
-  for (const [headerKey, separators] of Object.entries(headerSeparators)) {
-    const bufferStart = packetHeaderStart + separators[0];
-    const bufferEnd = packetHeaderStart + separators[1];
-    const headerBuffer = buffer.slice(bufferStart, bufferEnd);
-    const byteLength = separators[1] - separators[0];
-    parsedHeaders[headerKey] = headerBuffer.readIntLE(0, byteLength);
-  }
-
-  return parsedHeaders;
-};
-
 // Ensure file is passed as input
 if (!process.argv[2]) {
   console.error("No file specified as argument");
   process.exit(1);
 }
 
+/**
+ *  Global header format according to pcap-savefile. Each row is 4 bytes, the boxes
+ *  in the split rows are two bytes each.
+ *            +------------------------------+
+ *            |        Magic number          |
+ *            +--------------+---------------+
+ *            |Major version | Minor version |
+ *            +--------------+---------------+
+ *            |      Time zone offset        |
+ *            +------------------------------+
+ *            |     Time stamp accuracy      |
+ *            +------------------------------+
+ *            |       Snapshot length        |
+ *            +------------------------------+
+ *            |   Link-layer header type     |
+ *            +------------------------------+
+ *
+ * */
+const globalHeaderSeparations = {
+  magicNumber: [0, 4],
+  majorVersion: [4, 6],
+  minorVersion: [6, 8],
+  timeZoneOffset: [8, 12],
+  timeStampAccuracy: [12, 16],
+  snapshotLength: [16, 20],
+  linkLayerHeaderType: [20, 24]
+};
+
+/* *  Packet header format according to pcap-savefile. Each row is 4 bytes
+ *              +----------------------------------------------+
+ *              |          Time stamp, seconds value           |
+ *              +----------------------------------------------+
+ *              |Time stamp, microseconds or nanoseconds value |
+ *              +----------------------------------------------+
+ *              |       Length of captured packet data         |
+ *              +----------------------------------------------+
+ *              |   Un-truncated length of the packet data     |
+ *              +----------------------------------------------+
+ * */
+const packetHeaderSeparations = {
+  timeStampSeconds: [0, 4],
+  timeStampMicro: [4, 8],
+  length: [8, 12],
+  lengthUntruncated: [12, 16]
+};
+
+const globalHeaderLength = 24;
+const packetHeaderLength = 16;
+
 // Parse file
 fs.readFile(process.argv[2], (err, buffer) => {
   if (err) throw err;
-  console.log(buffer);
+  const globalHeader = parseHeadersFromBuffer(buffer, globalHeaderSeparations);
+  if (globalHeader.linkLayerHeaderType === 1) {
+    console.log("Ethernet Link Layer Header Type detected");
+  }
 
-  const globalHeaders = parseGlobalHeader(buffer);
-  console.log(globalHeaders);
+  // start iterating on packets after the global headers
+  let position = globalHeaderLength;
+  let packetCount = 0;
 
-  const firstPacketHeaders = parsePacketHeader(buffer, 24);
-  console.log(firstPacketHeaders);
+  while (position < buffer.length) {
+    const packetHeader = parseHeadersFromBuffer(
+      buffer,
+      packetHeaderSeparations,
+      position
+    );
+    console.log(packetHeader);
+    position += packetHeaderLength + packetHeader.length;
+    packetCount += 1;
+  }
 
-  const secondPacketHeaders = parsePacketHeader(buffer, 118);
-  console.log(secondPacketHeaders);
+  console.log(`${packetCount} packets parsed`);
 });
